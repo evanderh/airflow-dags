275ccad078194e76c8310ffe9c9714b5812a6ed2
Eric VanderHelm <vanderhelm.eric@gmail.com>
2024-07-07 22:33:16 -0500

    extract daily

--- gfs_extract.py
+++ gfs_extract.py
@@ -19,7 +19,7 @@ BASE_URL = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_0p25.pl'
 with DAG(
     dag_id='gfs_extract',
     start_date=datetime(2024, 7, 1),
-    schedule='0 3,9,15,21 * * *',
+    schedule='@daily',
     catchup=False,
     user_defined_macros={
         'cycle_hour': cycle_hour,
